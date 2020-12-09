095d23051965bb9ccc77eb968e30450314070dc9
UIT19520625 <74457336+UIT19520625@users.noreply.github.com>
2020-12-09 22:18:41 +0700

    Add files via upload

--- Snake_Source.cpp
+++ Snake_Source.cpp
@@ -16,27 +16,6 @@ using namespace std;
 #define LEFT 3
 #define RIGHT 4
 
-//#define Top_Wall 0
-//#define Bottom_Wall 1080/20
-//#define Left_Wall 0
-//#define Right_Wall 1920/20
-
-
-// Get the horizontal and vertical screen sizes in pixel
-/*void GetDesktopResolution(int& horizontal, int& vertical)
-{
-	RECT desktop;
-	// Get a handle to the desktop window
-	const HWND hDesktop = GetDesktopWindow();
-	// Get the size of screen to the variable desktop
-	GetWindowRect(hDesktop, &desktop);
-	// The top left corner will have coordinates (0,0)
-	// and the bottom right corner will have coordinates
-	// (horizontal, vertical)
-	horizontal = desktop.right;
-	vertical = desktop.bottom;
-}*/
-
 struct ToaDo
 {
 	int x, y;
@@ -44,7 +23,7 @@ struct ToaDo
 
 ToaDo snake[MAX];
 ToaDo FakeFood[MAX];
-int numberOfDots = 11; // số nốt ban đầu của rắn
+int numberOfDots = 5; // số nốt ban đầu của rắn
 int direction = RIGHT; // khởi tạo hướng đi ban đầu 
 
 int point = 0;
@@ -53,21 +32,14 @@ int point = 0;
 // khởi tạo rắn
 void init_Snake(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 {
-	snake[0].x = Left_Wall + 11;
-	snake[1].x = Left_Wall + 10;
-	snake[2].x = Left_Wall + 9;
-	snake[3].x = Left_Wall + 8;
-	snake[4].x = Left_Wall + 7;
-	snake[5].x = Left_Wall + 6;
-	snake[6].x = Left_Wall + 5;
-	snake[7].x = Left_Wall + 4;
-	snake[8].x = Left_Wall + 3;
-	snake[9].x = Left_Wall + 2;
-	snake[10].x = Left_Wall + 1;
+	snake[0].x = Left_Wall + 5;
+	snake[1].x = Left_Wall + 4;
+	snake[2].x = Left_Wall + 3;
+	snake[3].x = Left_Wall + 2;
+	snake[4].x = Left_Wall + 1;
 	snake[0].y = Bottom_Wall / 2 + 1;
-	snake[1].y = snake[2].y = snake[3].y = snake[4].y = snake[5].y = snake[6].y = snake[7].y = snake[8].y = snake[9].y = snake[10].y = Bottom_Wall / 2 + 1;
+	snake[1].y = snake[2].y = snake[3].y = snake[4].y = Bottom_Wall / 2 + 1;
 }
-
 // hiển thị rắn
 void display_Snake(int direction, int SnakeX, int SnakeY, int SpeedX, int SpeedY)
 {
@@ -183,7 +155,6 @@ void display_Snake(int direction, int SnakeX, int SnakeY, int SpeedX, int SpeedY
 		break;
 	}
 }
-
 // di chuyển
 void move(int direct)
 {
@@ -212,7 +183,6 @@ void move(int direct)
 		break;
 	}
 }
-
 // đổi hướng theo phím bấm
 void do_events(int& direct, int& SnakeX, int& SnakeY)
 {
@@ -238,7 +208,6 @@ void do_events(int& direct, int& SnakeX, int& SnakeY)
 		SnakeY = snake[0].y;
 	}
 }
-
 // vẽ tường
 void draw_wall(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall) {
 	// draw corner
@@ -269,16 +238,15 @@ void draw_wall(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall) {
 	gotoXY(Right_Wall + 4, Top_Wall + 1);
 	cout << "*** NOTE ***";
 	gotoXY(Right_Wall + 4, Top_Wall + 3);
-	cout << "Green food = Death.";
+	cout << "Red food = Death.";
 	gotoXY(Right_Wall + 4, Top_Wall + 5);
-	cout << "Yellow food = Faster and longer.";
+	cout << "Green food = Increase point and become longer.";
 	gotoXY(Right_Wall + 4, Top_Wall + 7);
-	cout << "Red food = Slower and shorter";
+	cout << "Yellow food = Reduce point and become shorter.";
 	gotoXY(Left_Wall, Bottom_Wall + 1);
 	setTextColor(10);
 	cout << "Point: " << point << endl;
 }
-
 // kiểm tra gắn chết
 bool check_Gameover(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 {
@@ -294,10 +262,9 @@ bool check_Gameover(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall
 	// đụng tường phải
 	if (snake[0].x == Right_Wall)
 		return true;
-	// ăn dính fake food
-	//for (int i = 0; i < numberOfFakeFoods; i++)
-	//	if (snake[0].x == FakeFood[i].x && snake[0].y == FakeFood[i].y)
-	//		return true;
+	// Point < 0
+	if (point < 0)
+		return true;
 	// tự hủy
 	for (int i = 1; i < numberOfDots; i++)
 		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
@@ -305,8 +272,6 @@ bool check_Gameover(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall
 	return false;
 
 }
-
-
 // hiển thị mồi ăn đc, màu vàng
 ToaDo display_food(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 {
@@ -320,7 +285,7 @@ ToaDo display_food(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 		}
 	}
 	gotoXY(x, y);
-	setTextColor(6);
+	setTextColor(2);
 	cout << (char)149;
 	return ToaDo{ x, y };
 }
@@ -337,7 +302,7 @@ ToaDo display_FakeFood(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_W
 		}
 	}
 	gotoXY(x, y);
-	setTextColor(4);
+	setTextColor(6);
 	cout << (char)149;
 	return ToaDo{ x, y };
 }
@@ -353,15 +318,15 @@ ToaDo display_DeathFood(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_
 		}
 	}
 	gotoXY(x, y);
-	setTextColor(2);
+	setTextColor(4);
 	cout << (char)149;
 	return ToaDo{ x, y };
 }
 // rắn ăn mồi
 bool checkEatFood(ToaDo food, int& SpeedX, int& SpeedY) {
 	if (snake[0].x == food.x && snake[0].y == food.y) {
-		SpeedX -= 2;
-		SpeedY -= 6;
+		SpeedX += 2;
+		SpeedY += 2;
 		return true;
 	}
 	return false;
@@ -369,7 +334,7 @@ bool checkEatFood(ToaDo food, int& SpeedX, int& SpeedY) {
 bool checkEatFakeFood(ToaDo fake_food, int& SpeedX, int& SpeedY) {
 	if (snake[0].x == fake_food.x && snake[0].y == fake_food.y) {
 		SpeedX += 2;
-		SpeedY += 6;
+		SpeedY += 2;
 		return true;
 	}
 	return false;
@@ -386,7 +351,6 @@ void addDots()
 	snake[numberOfDots] = snake[numberOfDots - 1];
 	numberOfDots++;
 }
-
 void subDots()
 {
 	snake[numberOfDots] = snake[numberOfDots + 1];
@@ -394,9 +358,248 @@ void subDots()
 	gotoXY(snake[numberOfDots].x, snake[numberOfDots].y);
 	cout << " ";
 }
-
+void DrawGameName(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall) {
+	// S
+	gotoXY(Left_Wall + 16, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 16, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 16, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 16, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 17, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 17, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 17, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 18, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 18, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 18, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 19, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 19, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 19, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 19, Top_Wall + 10);
+	cout << char(219);
+	// N
+	gotoXY(Left_Wall + 21, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 21, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 21, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 21, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 21, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 22, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 23, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 24, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 24, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 24, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 24, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 24, Top_Wall + 10);
+	cout << char(219);
+	// A
+	gotoXY(Left_Wall + 26, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 26, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 26, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 26, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 27, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 27, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 28, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 28, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 29, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 29, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 29, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 29, Top_Wall + 10);
+	cout << char(219);
+	// K
+	gotoXY(Left_Wall + 31, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 31, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 31, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 31, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 31, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 32, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 33, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 33, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 34, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 34, Top_Wall + 10);
+	cout << char(219);
+	// E
+	gotoXY(Left_Wall + 36, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 36, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 36, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 36, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 36, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 37, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 37, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 37, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 38, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 38, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 38, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 39, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 39, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 39, Top_Wall + 10);
+	cout << char(219);
+	// G
+	gotoXY(Left_Wall + 44, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 44, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 44, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 44, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 44, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 45, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 45, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 46, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 46, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 46, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 47, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 47, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 47, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 47, Top_Wall + 10);
+	cout << char(219);
+	// A
+	gotoXY(Left_Wall + 49, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 49, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 49, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 49, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 50, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 50, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 51, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 51, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 52, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 52, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 52, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 52, Top_Wall + 10);
+	cout << char(219);
+	// M
+	gotoXY(Left_Wall + 54, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 54, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 54, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 54, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 55, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 56, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 56, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 57, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 58, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 58, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 58, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 58, Top_Wall + 10);
+	cout << char(219);
+	// E
+	gotoXY(Left_Wall + 60, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 60, Top_Wall + 7);
+	cout << char(219);
+	gotoXY(Left_Wall + 60, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 60, Top_Wall + 9);
+	cout << char(219);
+	gotoXY(Left_Wall + 60, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 61, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 61, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 61, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 62, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 62, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 62, Top_Wall + 10);
+	cout << char(219);
+	gotoXY(Left_Wall + 63, Top_Wall + 6);
+	cout << char(219);
+	gotoXY(Left_Wall + 63, Top_Wall + 8);
+	cout << char(219);
+	gotoXY(Left_Wall + 63, Top_Wall + 10);
+	cout << char(219);
+}
 bool Display_GameOver(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall) {
 	clrscr();
+	DrawGameName(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 	// draw corner
 	gotoXY(Left_Wall, Top_Wall);
 	cout << char(201);
@@ -420,6 +623,7 @@ bool Display_GameOver(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wa
 		gotoXY(x, Bottom_Wall);
 		cout << (char)205;
 	}
+
 	gotoXY(Right_Wall / 2 - 7, Bottom_Wall / 2 - 1);
 	cout << "GAME OVER!";
 	gotoXY(Right_Wall / 2 - 7, Bottom_Wall / 2 + 1);
@@ -437,11 +641,105 @@ bool Display_GameOver(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wa
 		return true;
 	return false;
 }
-
+void Display_GameStart(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall) {
+	clrscr();
+	DrawGameName(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
+	int Top = 18, Bottom = 23;
+	int LeftStart = 20, RightStart = 29;
+	int LeftHS = 33, RightHS = 46;
+	int LeftExit = 50, RightExit = 58;
+	//draw Start
+	gotoXY(LeftStart, Top);
+	cout << char(201);
+	gotoXY(LeftStart, Bottom);
+	cout << char(200);
+	gotoXY(RightStart, Bottom);
+	cout << char(188);
+	gotoXY(RightStart, Top);
+	cout << char(187);
+		// Draw left + right wall
+	for (int y = Top + 1; y <= Bottom - 1; y++) {
+		gotoXY(LeftStart, y);
+		cout << (char)186;
+		gotoXY(RightStart, y);
+		cout << (char)186;
+	}
+		//Draw Top + bottom wall
+	for (int x = LeftStart + 1; x <= RightStart - 1; x++) {
+		gotoXY(x, Top);
+		cout << (char)205;
+		gotoXY(x, Bottom);
+		cout << (char)205;
+	}
+	gotoXY(LeftStart + 2, Top + 2);
+	cout << "1.START";
+	//draw HighScore
+	gotoXY(LeftHS, Top);
+	cout << char(201);
+	gotoXY(LeftHS, Bottom);
+	cout << char(200);
+	gotoXY(RightHS, Bottom);
+	cout << char(188);
+	gotoXY(RightHS, Top);
+	cout << char(187);
+		// Draw left + right wall
+	for (int y = Top + 1; y <= Bottom - 1; y++) {
+		gotoXY(LeftHS, y);
+		cout << (char)186;
+		gotoXY(RightHS, y);
+		cout << (char)186;
+	}
+		//Draw Top + bottom wall
+	for (int x = LeftHS + 1; x <= RightHS - 1; x++) {
+		gotoXY(x, Top);
+		cout << (char)205;
+		gotoXY(x, Bottom);
+		cout << (char)205;
+	}
+	gotoXY(LeftHS + 2, Top + 2);
+	cout << "2.HIGHTSCORE";
+	//Draw Exit
+	gotoXY(LeftExit, Top);
+	cout << char(201);
+	gotoXY(LeftExit, Bottom);
+	cout << char(200);
+	gotoXY(RightExit, Bottom);
+	cout << char(188);
+	gotoXY(RightExit, Top);
+	cout << char(187);
+		// Draw left + right wall
+	for (int y = Top + 1; y <= Bottom - 1; y++) {
+		gotoXY(LeftExit, y);
+		cout << (char)186;
+		gotoXY(RightExit, y);
+		cout << (char)186;
+	}
+		//Draw Top + bottom wall
+	for (int x = LeftExit + 1; x <= RightExit - 1; x++) {
+		gotoXY(x, Top);
+		cout << (char)205;
+		gotoXY(x, Bottom);
+		cout << (char)205;
+	}
+	gotoXY(LeftExit + 2, Top + 2);
+	cout << "3.EXIT";
+	gotoXY(34, Bottom + 3);
+	cout << "PRESS NUMBER: ";
+	gotoXY(48, Bottom + 3);
+	int n;
+	cin >> n;
+	if (n == 1)
+		run_game(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
+	if (n == 2)
+		cout << point;
+	if (n == 3)
+		return;
+}
 // bắt đầu game
 void run_game(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 {
 	clrscr();
+	setTextColor(7);
 	init_Snake(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 	draw_wall(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 	ToaDo food = display_food(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);// hiển thị thức ăn và trả về tọa độ
@@ -467,18 +765,21 @@ void run_game(int Left_Wall, int Right_Wall, int Top_Wall, int Bottom_Wall)
 		{
 			fake_food = display_FakeFood(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 			subDots();
+			point--;
 		}
 		if (checkEatDeathFood(death_food) == true)
 			break;
 		if (check_Gameover(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall) == true) {
 			break;
 		}
+		setTextColor(7);
 	}
 	Sleep(500);
 	if (Display_GameOver(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall) == true) {
 		point = 0;
-		numberOfDots = 11;
-		return run_game(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
+		numberOfDots = 5;
+		direction = RIGHT;
+		run_game(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 	}
 	gotoXY(Left_Wall, Bottom_Wall + 1);
 }
@@ -488,10 +789,9 @@ int main()
 {
 	int Left_Wall = 0,
 		Top_Wall = 0,
-		Right_Wall = 80,
-		Bottom_Wall = 27;
-	//GetDesktopResolution(Right_Wall, Bottom_Wall);*/
-	run_game(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
+		Right_Wall = 80, 
+		Bottom_Wall = 30;
+	Display_GameStart(Left_Wall, Right_Wall, Top_Wall, Bottom_Wall);
 	system("Pause");
 	return 0;
 }
